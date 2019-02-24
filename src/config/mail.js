1ca32433d434d6270dc10faab74f90a01bc29c6d
wagner <wagnerricardonet@gmail.com>
2019-02-24 15:31:09 -0300

    utilizando variáveis de ambiente no node

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,9 +1,9 @@
 module.exports = {
-  host: "smtp.mailtrap.io",
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: "a1c7c1df02a814",
-    pass: "052169f2d8257d"
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS
   }
 };
